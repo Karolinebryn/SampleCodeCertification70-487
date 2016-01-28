1c26bfaafcabcb89371dccbc98833d2226014b1f
Karolinebryn <karolinebrynildsen@gmail.com>
2016-01-28 11:52:48 +0100

    added "using the dom model"

---
+++ CertificationTestSolution/LinqToXml/Pages/UsingTheDomModel.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UsingTheDomModel.aspx.cs" Inherits="LinqToXml.Pages.UsingTheDomModel" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        
+        <div class="col-md-12">
+            <h2>Process XML Data Using the DOM Model</h2>
+            <p>
+                The XML Document Object Model (DOM) treats XML data as a standard set of objects and is used to process XML data in memory. 
+                The System.Xml namespace provides a programmatic representation of XML documents, fragments, nodes, or node-sets. 
+                It is based on the World Wide Web Consortium (W3C) DOM Level 1 Core and the DOM Level 2 Core recommendations. 
+                The XmlDocument class represents an XML document. It includes members for retrieving and creating all other XML objects. 
+                Using the XmlDocument, and its related classes, you can construct XML documents, load and access data, modify data, 
+                and save changes.
+            </p>
+            <p>
+                If an application does not require the structure or editing capabilities provided by the DOM, the XmlReader and XmlWriter classes provide non-cached, forward-only stream access to XML.
+                 For fast, non-cached, forward-only stream access to XML, use the XmlReader and XmlWriter.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Create a new XmlDocument</h3>
+            <asp:Label runat="server" ID="lblResult1"></asp:Label>
+        </div>
+        <div class="col-md-6">
+            <h3>Read XML into DOM with XmlDocument</h3>
+            <asp:Label runat="server" ID="lblResult2"></asp:Label>
+        </div>
+        <div class="col-md-6">
+            <h3>Accessing Attributes In The Dom</h3>
+            <asp:Literal runat="server" ID="literalResult3"></asp:Literal>
+        </div>
+        <div class="col-md-6">
+            <h3>Remove node and attribute from the DOM</h3>
+            <p><b>Original XML</b></p>
+            <textarea runat="server" ID="taBefore" style="width: 100%;height: 100px;"></textarea>
+            <p><b>Node and attribute is removed</b></p>
+            <textarea runat="server" ID="taAfter" style="width: 100%;height: 100px;"></textarea>
+        </div>
+    </div>
+</asp:Content>
---
+++ CertificationTestSolution/LinqToXml/Pages/UsingTheDomModel.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace LinqToXml.Pages
+{
+    public partial class UsingTheDomModel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                CreateXmlDocument();
+                ReadXmlDocumentIntoDom();
+                AccessAttributesInTheDom();
+                RemoveNodeAndAttributeFromTheDom();
+            }
+        }
+
+        private void CreateXmlDocument()
+        {
+            try
+            {
+                var document = new XmlDocument();
+                var root = document.CreateElement("Items");
+                var item1 = document.CreateElement("Item");
+                var item1text = document.CreateTextNode("item 1");
+
+                //set attribute1
+                item1.SetAttribute("attr1", "thisisanattribute");
+
+                //set attribute2
+                XmlAttribute attr2 = document.CreateAttribute("attr2");
+                attr2.Value = "thisisanotherattribute";
+                item1.SetAttributeNode(attr2);
+
+                root.AppendChild(item1);
+                root.LastChild.AppendChild(item1text);
+                document.AppendChild(root);
+
+                document.Save(@"C:\TestDoc\test1.xml");
+
+                lblResult1.Text = "Document successfully saved to path: C:\\TestDoc\\test1.xml";
+                lblResult1.ForeColor = System.Drawing.Color.Black;
+            }
+            catch(Exception ex)
+            {
+                lblResult1.Text = ex.Message;
+                lblResult1.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void ReadXmlDocumentIntoDom()
+        {
+            try
+            {
+                //from string
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml("<book genre='novel' ISBN='1-861001-57-5'>" +
+                            "<title>Pride And Prejudice</title></book>");
+
+                //from file
+                var path = Server.MapPath("~/App_Data/books.xml");
+                XmlDocument doc2 = new XmlDocument();
+                doc2.Load(path);
+
+                lblResult2.Text = "Documents successfully loaded into DOM.";
+                lblResult2.ForeColor = System.Drawing.Color.Black;
+            }
+            catch (Exception ex)
+            {
+                lblResult2.Text = ex.Message;
+                lblResult2.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void AccessAttributesInTheDom()
+        {
+            var output = "";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<book genre='novel' ISBN='1-861001-57-5' misc='sale item'>" +
+                          "<title>The Handmaid's Tale</title>" +
+                          "<price>14.95</price>" +
+                          "</book>");
+
+            // Move to an element.
+            XmlElement myElement = doc.DocumentElement;
+
+            // Create an attribute collection from the element.
+            XmlAttributeCollection attrColl = myElement.Attributes;
+
+            // Show the collection by iterating over it.
+            output += "<p><b>Display all the attributes in the collection:</b><br />";
+            for (int i = 0; i < attrColl.Count; i++)
+            {
+                output += string.Format("{0} = ", attrColl[i].Name);
+                output += string.Format("{0}<br />", attrColl[i].Value);
+            }
+            output += "</p>";
+
+            // Retrieve a single attribute from the collection; specifically, the
+            // attribute with the name "misc".
+            XmlAttribute attr = attrColl["misc"];
+
+            // Retrieve the value from that attribute.
+            var miscValue = attr.InnerXml;
+
+            output += "<p><b>Display one attribute information:</b><br />";
+            output += miscValue + "</p>";
+
+            literalResult3.Text = output;
+        }
+
+        private void RemoveNodeAndAttributeFromTheDom()
+        {
+            var path = Server.MapPath("~/App_Data/books.xml");
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<books genre='novel'>" +
+                    "<book subgenra='lovenovel' ISBN='1-861001-57-5'><title>Pride And Prejudice</title></book>" +
+                    "<book subgenra='mystery' ISBN='1-861001-57-5'><title>The DaVinci Code</title></book>" +
+                            "</books>");
+
+            var root = doc.DocumentElement;
+            taBefore.InnerHtml = root.OuterXml;
+
+            //Remove the first child
+            root.RemoveChild(root.FirstChild);
+            root.RemoveAttribute("genre");
+
+            taAfter.InnerHtml = root.OuterXml;
+        }
+
+    }
+}
