a127308570a6b996020f6ceb0992e431528de008
Karolinebryn <karolinebrynildsen@gmail.com>
2016-01-29 12:45:20 +0100

    blob storage sample code

--- CertificationTestSolution/AzureDataStorage/Pages/BlobStorage.aspx
+++ CertificationTestSolution/AzureDataStorage/Pages/BlobStorage.aspx
@@ -1,3 +1,42 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BlobStorage.aspx.cs" Inherits="AzureDataStorage.Pages.BlobStorage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Azure Blob Storage</h2>
+            <asp:Label runat="server" ID="lblErrorMsg" ForeColor="Red"></asp:Label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Select Blob Container</h3>
+            <label>Existing containers</label><br />
+            <asp:Repeater runat="server" ID="rpExistingContainers">
+                <HeaderTemplate>
+                    <p>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <asp:Label runat="server" text='<%# Eval("Name") %>' /> <br />
+                </ItemTemplate>
+                <FooterTemplate>
+                    </p>
+                </FooterTemplate>
+            </asp:Repeater>
+            <div class="form-group">
+                <label>Container name </label>
+                <asp:TextBox runat="server" ID="tbContainerName" CssClass="form-control"></asp:TextBox>
+            </div>
+            <asp:Button runat="server" ID="btnSelectContainer" Text="Select" OnClick="btnSelectContainer_Click" CssClass="btn btn-primary" />
+            <asp:Label runat="server" ID="lblCreateContainerResult"></asp:Label>
+        </div>
+        <div class="col-md-6">
+            <h3>Upload to Blob Container</h3>
+            <asp:FileUpload runat="server" ID="fileUploadToContainer" AllowMultiple="false" />
+            <asp:Button runat="server" ID="btnUploadToContainer" Text="Upload" OnClick="btnUploadToContainer_Click" CssClass="btn btn-primary" />
+            <asp:Label ID="lblUploadStatus" runat="server"></asp:Label>
+        </div>
+        <div class="col-md-6">
+            <h3>All blobs in file upload container</h3>
+            <asp:Literal runat="server" ID="literalBlobsInContainer"></asp:Literal>
+        </div>
+    </div>
 </asp:Content>
--- CertificationTestSolution/AzureDataStorage/Pages/BlobStorage.aspx.cs
+++ CertificationTestSolution/AzureDataStorage/Pages/BlobStorage.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,124 @@ namespace AzureDataStorage.Pages
 {
     public partial class BlobStorage : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                FillExistingContainersLists();
+                FillBlobsInContainerList();
+            }
+        }
 
+        private void FillBlobsInContainerList()
+        {
+            var blobClient = InitBlobClient();
+            var container = blobClient.GetContainerReference("upload-files");
+            var output = "<ul>";
+
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {            
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    output += "<li> Block blob of length " + blob.Properties.Length + ": " + blob.Uri + "</li>";
+
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob pageBlob = (CloudPageBlob)item;
+                    output += "<li>Page blob of length " + pageBlob.Properties.Length + ": " + pageBlob.Uri + "</li>";
+
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory directory = (CloudBlobDirectory)item;
+                    output += "<li>Directory: " + directory.Uri + "</li>";
+                }
+            }
+            output += "</ul>";
+            literalBlobsInContainer.Text = output;
+        }
+
+        private CloudBlobClient InitBlobClient()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            return blobClient;
+        }
+
+        protected void btnSelectContainer_Click(object sender, EventArgs e)
+        {
+            lblCreateContainerResult.Text = "";
+            var containerName = tbContainerName.Text;
+
+            try
+            {
+                var blobClient = InitBlobClient();
+                var container = SelectBlobContainer(blobClient, containerName);
+
+                tbContainerName.Text = "";
+                lblCreateContainerResult.Text = "Container '" + containerName + "' is selected!";
+            }
+            catch (Exception ex)
+            {
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        private CloudBlobContainer SelectBlobContainer(CloudBlobClient blobClient, string containerName)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            var wasCreated = container.CreateIfNotExists();
+
+            if (wasCreated)
+            {
+                container.SetPermissions(new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+
+                FillExistingContainersLists();
+            }
+            return container;
+        }
+
+        private void FillExistingContainersLists()
+        {
+            try
+            {
+                var blobClient = InitBlobClient();
+                var containers = blobClient.ListContainers();
+                rpExistingContainers.DataSource = containers;
+                rpExistingContainers.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        protected void btnUploadToContainer_Click(object sender, EventArgs e)
+        {
+            if (fileUploadToContainer.HasFile)
+            {
+                try
+                {
+                    var blobClient = InitBlobClient();
+                    var container = blobClient.GetContainerReference("upload-files");
+                    var blockBlob = container.GetBlockBlobReference(fileUploadToContainer.FileName);
+
+                    var stream = fileUploadToContainer.PostedFile.InputStream;
+                    blockBlob.UploadFromStream(stream);
+                    lblUploadStatus.Text = "File successfully added to blob storage";
+                }
+                catch (Exception ex)
+                {
+                    lblUploadStatus.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
         }
     }
 }
