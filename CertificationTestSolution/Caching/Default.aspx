25b26aa2e7d24b246cafa1e014668f6e63a23bc0
Karolinebryn <karolinebrynildsen@gmail.com>
2016-01-29 09:31:38 +0100

    added sample caching project

---
+++ CertificationTestSolution/Caching/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Caching._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+<div class="row">
+    <div class="col-md-12">
+        <h1>Caching</h1>
+    </div>
+    <div class="col-md-12">
+        <h2>Cache.Add()</h2>
+        <p>Cache.Add() will do nothing if key exists in cache.</p>
+        <asp:Button runat="server" OnClick="AddItemToCache" Text="Add Item to Cache" CssClass="btn btn-primary" />
+        <asp:Button runat="server" OnClick="RemoveItemFromCache" Text="Remove Item from Cache" CssClass="btn btn-primary" />
+        <div>
+            <b>Status: </b>
+            <asp:Label runat="server" ID="lblResult"></asp:Label>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <h2>Cache.Insert()</h2>
+        <p>Cache.Insert() will overwrite value in cache if key exists.</p>
+        <asp:Button runat="server" OnClick="InsertItemToCache" Text="Insert Item to Cache" CssClass="btn btn-primary" />
+        <div>
+            <b>Status: </b>
+            <asp:Label runat="server" ID="lblResult2"></asp:Label>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <h2>CacheDependency</h2>
+        <p>
+            To test; click "Insert" button, then click "Check" button. The item should still exist in cache.
+            Then open document (AppData/books.xml), change something and save it. Click "Check" button again.
+            Item should no longer be in cache.
+        </p>
+        <asp:Button runat="server" OnClick="InsertItemWithDependency" Text="Insert Item to Cache" CssClass="btn btn-primary" />
+        <asp:Button runat="server" OnClick="CheckIfItemWithDependencyExists" Text="Check if item is in Cache" CssClass="btn btn-primary" />
+
+        <div>
+            <b>Status: </b>
+            <asp:Label runat="server" ID="lblResult3"></asp:Label>
+        </div>
+    </div>
+    
+</div>
+
+</asp:Content>
---
+++ CertificationTestSolution/Caching/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Web.Caching;
+using System.Web.UI;
+using System.Xml;
+
+namespace Caching
+{
+    public partial class _Default : Page
+    {
+        static int _counter = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        static bool itemRemoved = false;
+        static CacheItemRemovedReason reason;
+        CacheItemRemovedCallback onRemove = null;
+
+        //TOTEST: set breakpoint here
+        //Run solution and click AddItemToCache button
+        //Either click RemoveItemFromCache button OR wait 60+ seconds and see that this event is fired
+        public void RemovedCallback(string k, object v, CacheItemRemovedReason r)
+        {
+            itemRemoved = true;
+            reason = r;
+            //Callback cannot access placeholders in aspx file
+            //lblResult.Text = "RemovedCallback event raised. Reason: " + reason;
+        }
+
+        public void AddItemToCache(Object sender, EventArgs e)
+        {
+            itemRemoved = false;
+
+            onRemove = new CacheItemRemovedCallback(RemovedCallback);
+
+            if (Cache["Key1"] == null)
+                Cache.Add("Key1", "Value 1", null, DateTime.Now.AddSeconds(60), Cache.NoSlidingExpiration, CacheItemPriority.High, onRemove);
+
+            lblResult.Text = "Value of cache key: " + Cache["Key1"];
+        }
+
+        public void RemoveItemFromCache(Object sender, EventArgs e)
+        {
+            if (Cache["Key1"] != null)
+                Cache.Remove("Key1");
+            lblResult.Text = "Removing from cache";
+        }
+
+        public void InsertItemToCache(Object sender, EventArgs e)
+        {
+            Cache.Insert("Key2", "Value 2 counter: " + _counter);
+            var item = Cache.Get("Key2");
+            lblResult2.Text = "Inserting item to cache: " + item;
+            _counter++;
+        }
+
+        public void InsertItemWithDependency(Object sender, EventArgs e)
+        {
+            var uri = Server.MapPath("~/App_Data/books.xml");
+            var document = new XmlDocument();
+            document.Load(uri);
+
+            var dependency = new CacheDependency(uri);
+            Cache.Insert("Books", document, dependency);
+
+            if (dependency.HasChanged)
+            {
+                //load document again
+            }
+
+            lblResult3.Text = "Books is added to cache with dependency";
+        }
+
+        private void CheckIfBooksIsInCache()
+        {
+            if (Cache["Books"] != null)
+                lblResult3.Text = "Books is still in cache";
+            else
+                lblResult3.Text = "Books is not in cache";
+        }
+
+        public void CheckIfItemWithDependencyExists(Object sender, EventArgs e)
+        {
+            CheckIfBooksIsInCache();
+        }
+    }
+}
