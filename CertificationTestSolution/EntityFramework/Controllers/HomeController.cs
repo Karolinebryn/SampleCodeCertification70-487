97846ab5a22e6358f8436f18961a8eac7b6a333f
Karolinebryn <karolinebrynildsen@gmail.com>
2016-02-01 09:57:26 +0100

    -

--- CertificationTestSolution/EntityFramework/Controllers/HomeController.cs
+++ CertificationTestSolution/EntityFramework/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityFramework.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,20 +11,7 @@ namespace EntityFramework.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            var test = new AdventureWorksEntities();
             return View();
         }
     }
