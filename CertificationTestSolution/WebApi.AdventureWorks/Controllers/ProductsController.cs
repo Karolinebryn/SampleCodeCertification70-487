f64c3fe55162d18d830d0354626ebdd874e473b8
Karolinebryn <karolinebrynildsen@gmail.com>
2016-02-04 15:44:12 +0100

    .

---
+++ CertificationTestSolution/WebApi.AdventureWorks/Controllers/ProductsController.cs
@@ -0,0 +1,126 @@
+﻿using Data.AdventureWorks;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApi.AdventureWorks.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private AdventureWorksEntities _context;
+
+        public ProductsController()
+        {
+            _context = new AdventureWorksEntities();
+        }
+
+        // GET: api/Products
+        public object Get()
+        {
+            try
+            {
+                return new
+                {
+                    Data = _context.Products.OrderByDescending(p => p.ModifiedDate).Take(10).Select(p => new { p.ProductID, p.Name, p.ListPrice }),
+                    Success = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    Success = false,
+                    ErrorMessage = ex.Message
+                };
+            }
+        }
+
+        // GET: api/Products/5
+        public object Get(int id)
+        {
+            try
+            {
+                var product = _context.Products.Find(id);
+
+                return new
+                {
+                    Data = new { product.ProductID, product.Name, product.ListPrice },
+                    Success = true
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    Success = false,
+                    ErrorMessage = ex.Message
+                };
+            }
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT: api/Products/5
+        public object Put(int id, Product product)
+        {
+            try
+            {
+                var existingProduct = _context.Products.Find(id);
+                existingProduct.Name = product.Name;
+                existingProduct.ListPrice = product.ListPrice;
+
+                _context.SaveChanges();
+
+                return new
+                {
+                    Success = true
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    Success = false,
+                    ErrorMessage = ex.Message
+                };
+            }
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+
+        }
+
+        [System.Web.Http.Route("async/products/{id}")]
+        public async Task<object> GetAsync(int id)
+        {
+            try
+            {
+                var product = await _context.Products.FindAsync(id);
+                return new
+                {
+                    Data = new { product.ProductID, product.Name, product.ListPrice },
+                    Success = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    Success = false,
+                    ErrorMessage = ex.Message
+                };
+            }
+        }
+
+    }
+}
---
+++ CertificationTestSolution/WebApi.AdventureWorks/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.AdventureWorks.Models
+{
+    public class Result
+    {
+        public string ErrorMessage { get; set; }
+        public bool Success { get; set; }
+        public object Data { get; set; }
+    }
+}
