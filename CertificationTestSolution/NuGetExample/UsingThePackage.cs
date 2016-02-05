02b95fab689f433433fe98071307a954b971ac62
Karolinebryn <karolinebrynildsen@gmail.com>
2016-02-05 12:36:28 +0100

    NuGetPackageExample: contains the project for an assembly that will be added to nuget. NuGetExample: the nuget service where .nupkg files must be added.

---
+++ CertificationTestSolution/NuGetExample/UsingThePackage.cs
@@ -0,0 +1,19 @@
+﻿using NuGetPackageExample; //This is the package created in project NuGetPackageExample and .nupkg file is added to ~/Package folder in this project and installed using NuGet B-)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NuGetExample
+{
+    public class UsingThePackage
+    {
+        public UsingThePackage()
+        {
+            //YAAAAAAAAAAAAAAAAAS
+            var calculator = new Calculator();
+            var result1 = calculator.Add(4);
+            var result2 = calculator.Substract(1);
+        }
+    }
+}
---
+++ CertificationTestSolution/NuGetPackageExample/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackageExample
+{
+    public class Calculator
+    {
+        private int _result = 0;
+
+        public int Result { get { return _result; } }
+
+        public int Add(int number)
+        {
+            return _result = _result + number;
+        }
+
+        public int Substract(int number)
+        {
+            return _result = _result - number;
+        }
+    }
+}
